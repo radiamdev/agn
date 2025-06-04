e0501069755f6e51281e07a749d94c4f8345634f
santatra <mariosantatra@gmail.com>
2025-06-04 14:05:07 +0300

    merge from dev

---
+++ app/termsOfUse/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./section/Hero"
+
+function page() {
+  return (
+    <div className="w-screen pt-[90px] md:pt-[150px]" >
+      <Hero />
+    </div>
+  )
+}
+
+export default page
